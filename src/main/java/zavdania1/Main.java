52b722cccde0536349304ca08754751b01227f0d
glebboxik-cmyk <glebboxik@gmail.com>
2026-03-12 19:41:10 +0200

    Update Main.java

--- src/main/java/zavdania1/Main.java
+++ src/main/java/zavdania1/Main.java
@@ -4,8 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); // Создаем инструмент для чтения
-
+        Scanner sc = new Scanner(System.in); 
         System.out.print("Vvedit rechenia: ");
         String input = sc.nextLine();
 
